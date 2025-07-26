aecd06f2bae4746549e5405e4ae910813bcca890
Eric Busboom <eric@busboom.org>
2025-07-25 18:06:48 -0700

    Add initial project setup with dependencies and basic test implementation
    - Created package.json with pxt-microbit dependency
    - Added main.ts file for future code implementation
    - Implemented a basic test in test.ts to display heart icons in a loop

--- Makefile
+++ Makefile
@@ -1,10 +1,15 @@
-all: deploy
+# Initial setup 
+setup: 
+	npm install -g pxt
+	pxt target microbit
+	pxt install
 
 build:
-	pxt build
+	PXT_FORCE_LOCAL=1 pxt build
+
 
 deploy:
-	pxt deploy
+	PXT_FORCE_LOCAL=1 pxt deploy
 
 test:
 	pxt test
---
+++ src/test.ts
@@ -0,0 +1,8 @@
+// tests go here; this will not be compiled when this package is used as an extension.
+basic.forever(function () {
+    basic.showIcon(IconNames.SmallHeart)
+    basic.pause(500)
+    basic.showIcon(IconNames.Heart)
+    basic.pause(500)
+})
+
