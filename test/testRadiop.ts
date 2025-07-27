05972fbd0852414db28ef46333ad273322b973fa
Eric Busboom <eric@busboom.org>
2025-07-27 14:24:01 -0700

    Fix formatting in serial output for message rate tracking in testRecvPayload function

--- test/testRadiop.ts
+++ test/testRadiop.ts
@@ -13,6 +13,6 @@ function testRecvPayload() {
         let elapsedSeconds = (currentTime - startTime) / 1000;
         messagesPerSecond = Math.round(messageCount / elapsedSeconds * 100) / 100; // Round to 2 decimal places
         lastRateUpdate = currentTime;
-        serial.writeLine("" + messageCount+ " " + messagesPerSecond + " m/s" + payload.str);
+        serial.writeLine("" + messageCount+ " " + messagesPerSecond + " m/s " + payload.str);
     });
 }
